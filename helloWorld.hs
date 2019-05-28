a28c8aedc0761716d172c3cae09c256506a40e5d
jRobinson33 <eldringethmoore@yahoo.com>
2019-05-28 15:56:35 -0700

    first commit

---
+++ helloWorld.hs
@@ -0,0 +1,22 @@
+{- 
+	Hello world in Haskell
+	5/28/2019
+	Using CMD line, navigate to the folder holding the program
+	
+	Start the compiler
+	type "ghci"
+	
+	load the program
+	type ":l helloWorld.hs"
+	
+	run the program
+	type "main"
+	
+	
+	Alternatively
+	compile with "ghc -o hello helloWorld.hs"
+	run in CMD line by typing "hello"
+-}
+
+main = putStrLn "Hello World"
+
