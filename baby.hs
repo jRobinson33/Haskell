c74e2a5137989319549d49674792d3a6590bdea7
jRobinson33 <eldringethmoore@yahoo.com>
2019-06-05 10:31:54 -0700

    added tuples examples to baby.hs

--- baby.hs
+++ baby.hs
@@ -1,6 +1,7 @@
 --From learnyouahaskell.com
 --5/29/2019
 
+{- first functions -}
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
@@ -9,6 +10,7 @@ doubleSmallNumber x = if x > 100
 then x
 else x*2
 
+{- intro to lists -}
 --Putting 2 lists together
 --using the ++ concatenation operator
 list1 = [1,2,3,4]
@@ -73,6 +75,8 @@ productList2 = product list2
 -- elem takes a thing and a list of things, and returns boolean value of if the thing is in the list
 elemList1 = elem 3 list1
 
+{- ranges -}
+
 -- can make a list from some early point to a later point
 -- letters 'A' 'Z' produces list inclusive of A to Z
 -- numbers 0 100 produces a list inclusive of 0 to 100
@@ -92,6 +96,8 @@ repeatDemo = take 12 (repeat 10)
 
 --similar to repeat, replicate makes a finite list using replicate "num of times" "thing to replicate"
 replicateDemo = replicate 10 20
+
+{-list comprehension-}
 
 --we can use a set notation
 set1 = [2*x | x <- [1..10]]
@@ -113,3 +119,40 @@ set6 = [ x*y | x <- [2,5,10], y <-[8,10,11] , x*y > 50]
 nouns = ["hobo","frog","pope"]
 adjectives = ["lazy","grouchy","scheming"]
 combos = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]
+
+--our own version of length
+{-
+_ means that we don't care what we'll draw from the list anyway so instead of writing a variable name
+we'll never use, we just write _. This function replaces every element of a list with 1 and then sums
+that up
+-}
+length' xs = sum[1 | _ <- xs]
+
+removeNonUppercase st = [c | c <- st, elem c ['A'..'Z']]
+
+{- TUPLES -}
+
+tup = (8,11)
+tup2 = ("Wow", False)
+
+--fst returns the first component of a tuple
+tupFst = fst tup
+tup2Fst = fst tup2
+
+--snd returns the second compononet of a tuple
+tupSnd = snd tup
+tup2Snd = snd tup2
+
+{- the above fst and snd functions only work on pairs "tuples of 2" -}
+
+{- zip takes 2 lists, and creates tuples based on element numbers of the lists -}
+zipRNounsAdj = zip nouns adjectives
+
+triangles = [(a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10]]
+
+rightTriangles = [(a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+
+rightTriangles' = [(a,b,c) | c <- [1..100], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c >= 24]
+
+
+
